d62175398f0f366f945aa0b85f02fac154d6ac25
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-04-25 13:42:45 +0200

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+function that take two strings and concate
+them
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function: concat
+    arguments: str1, str2
+    return: a string value
+    """
+    return str1 + str2
