4bbbb8bf08ede7adb59482926bfc577633cd6c7f
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-04-25 14:21:18 +0200

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+this function make an string representation
+return the argument as string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    function: to_str
+    arguments: n
+    return: str
+    """
+    return str(n)
