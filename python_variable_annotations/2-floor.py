c41d97ba6549f788f17e00b2d28da1a8b9c53201
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-04-25 13:48:03 +0200

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+function that returns a floor number
+takes one float number as argument
+"""
+
+
+def floor(n: float) -> int:
+    """
+    function: floor
+    argument: n
+    retun: integer
+    """
+    return int(n)
