377bf70539776b6bbf722c36f2c33f63f24f3761
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-04-26 12:02:50 +0200

    task 0

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+this module implements a method that get back random
+numbers gereted by the asynchronous random numbers generator
+"""
+import asyncio
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    async_comprehension function
+    no arguments
+    return List of floats
+    """
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
