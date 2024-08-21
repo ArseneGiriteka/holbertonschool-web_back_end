3b97a263fb5d03a7c7c4efcdae2c7ee23d7f852e
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-08-21 15:40:02 +0200

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,6 +8,7 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """
     This function is an async generator that yields
