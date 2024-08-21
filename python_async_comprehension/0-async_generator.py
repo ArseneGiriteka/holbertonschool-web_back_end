f62cea2d34ad391c9bb7743710b0e3fa8d04b2d2
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-08-21 15:00:30 +0200

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,11 @@
 import asyncio
 import random
 import typing
+"""This module contains an async generator that yields"""
 
 
 async def async_generator() -> typing.AsyncGenerator[float, None]:
+    """this function is an async generator that yields"""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
