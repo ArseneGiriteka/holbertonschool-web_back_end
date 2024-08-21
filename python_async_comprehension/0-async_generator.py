918e8bbd8d769df860402aff631746697b2a52bd
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-08-21 15:29:20 +0200

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,8 +3,17 @@ import asyncio
 import random
 import typing
 
+"""
+This module contains an async generator that yields
+random numbers between 0 and 10 every second for 10 seconds.
+"""
+
 
 async def async_generator() -> typing.AsyncGenerator[float, None]:
+    """
+    This function is an async generator that yields
+    random numbers between 0 and 10 every second for 10 seconds.
+    """
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
