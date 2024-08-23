c16fac52407236a2850ce6f492a4d2197f412627
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-08-23 23:04:18 +0200

    fixing

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 """
-This module provides a function to insert a document into a MongoDB collection.
+This module provides a function to insert a document
+into a MongoDB collection.
 """
 
 import pymongo
+
 
 def insert_school(mongo_collection, **kwargs):
     """
     Insert a document into the specified MongoDB collection.
 
     Args:
-        mongo_collection (Collection): The pymongo collection object where the document will be inserted.
-        **kwargs: Keyword arguments representing the fields and values of the document to be inserted.
+        mongo_collection (Collection): The pymongo
+        collection object where the document will be inserted.
+        **kwargs: Keyword arguments representing the
+        fields and values of the document to be inserted.
 
     Returns:
         str: The _id of the newly inserted document as a string.
