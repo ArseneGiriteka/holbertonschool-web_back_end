e85bca3446405c05f310e2dc9a7faf7afdf4c03e
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-08-23 23:28:16 +0200

    task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This update all topics
+and return nothing
+"""
+
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    This function takes a collection,
+    a name(string) and a list of topics(string)
+    to update
+    """
+    mongo_collection.update({"name": name},
+                            {'$set': {"topics": topics}}, multi=True)
