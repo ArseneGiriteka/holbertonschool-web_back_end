dd4b20e44461c1aa5123f2585476a0bf7c4c3d7e
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-08-23 22:46:25 +0200

    task 9

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 """
-this is a script to insert doc
-and return result
+This script defines a function to insert a document
+into a MongoDB collection.
 """
 
 import pymongo
+from pymongo.collection import Collection
 
 
-def insert_school(mongo_collection, **kwargs):
+def insert_school(mongo_collection: Collection, **kwargs) -> str:
     """
-    this function insert a doc in
-    a given collection
+    Insert a document into the specified MongoDB collection.
+
+    Args:
+        mongo_collection (Collection): The pymongo collection
+        object where the document will be inserted.
+        **kwargs: Keyword arguments representing the fields and
+        values of the document to be inserted.
+
+    Returns:
+        str: The _id of the newly inserted document as a string.
     """
-    querry_result = mongo_collection.insert_one(kwargs)
-    return str(querry_result.inserted_id)
+
+    query_result = mongo_collection.insert_one(kwargs)
+
+    return str(query_result.inserted_id)
