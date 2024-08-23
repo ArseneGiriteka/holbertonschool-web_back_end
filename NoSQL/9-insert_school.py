eb277aeab039966cd92448a2cddc65c3798f801b
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-08-23 22:58:50 +0200

    task 9

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -4,14 +4,9 @@ this is a script to insert doc
 and return result
 """
 
-from typing import List, Dict
-from pymongo.collection import Collection
+import pymongo
 
 
-def insert_school(mongo_collection: Collection, **kwargs: List[Dict]) -> str:
-    """
-    this function insert a doc in
-    a given collection
-    """
+def insert_school(mongo_collection, **kwargs):
     query_result = mongo_collection.insert_one(kwargs)
     return str(query_result.inserted_id)
