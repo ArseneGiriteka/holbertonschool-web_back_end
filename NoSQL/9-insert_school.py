ca0c41a41b1580e08b944c51f803b303a617fab1
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-08-23 23:18:16 +0200

    inserted

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -10,15 +10,7 @@ import pymongo
 def insert_school(mongo_collection, **kwargs):
     """
     Insert a document into the specified MongoDB collection.
-
-    Args:
-        mongo_collection (Collection): The pymongo
-        collection object where the document will be inserted.
-        **kwargs: Keyword arguments representing the
-        fields and values of the document to be inserted.
-
-    Returns:
-        str: The _id of the newly inserted document as a string.
+    and return id inserted
     """
     query_result = mongo_collection.insert_one(kwargs)
-    return str(query_result.inserted_id)
+    return query_result.inserted_id
