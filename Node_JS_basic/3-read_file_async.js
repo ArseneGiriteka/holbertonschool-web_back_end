4a8e3ef6464f38e3f1f4dafadde8a9251102013f
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-09-20 15:47:38 +0200

    task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,44 @@
+const promise = require('fs').promises;
+
+async function countStudents(path) {
+    try {
+      const data = await promise.readFile(path, 'utf-8');
+      const rows = data.trim().split('\n');
+      rows.shift();
+      console.log(`Number of students: ${rows.length}`);
+  
+      const fields = [];
+  
+      for (let i = 0; i < rows.length; i += 1) {
+        const field = rows[i].split(',')[3].trim();
+        if (!(fields.includes(field))) {
+          fields.push(field);
+        }
+      }
+  
+      for (const field of fields) {
+        const names = [];
+        for (const row of rows) {
+          if (field === row.trim().split(',')[3].trim()) {
+            names.push(row.trim().split(',')[0].trim());
+          }
+        }
+  
+        let strNames = '';
+        for (let i = 0; i < names.length; i += 1) {
+          if (names.length === 0) {
+            strNames = '';
+          } else if (i !== names.length - 1) {
+            strNames += ` ${names[i]},`;
+          } else {
+            strNames += ` ${names[i]}`;
+          }
+        }
+        console.log(`Number of students in ${field}: ${names.length}. List:${strNames}`);
+      }
+    } catch (error) {
+      throw new Error('Cannot load the database');
+    }
+  }
+  
+  module.exports = countStudents;
