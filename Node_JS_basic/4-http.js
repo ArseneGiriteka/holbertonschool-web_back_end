da9b7a80e4a822006c99ff0a4bea74ae77926875
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-09-20 16:03:12 +0200

    task 4 done

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const app = http.createServer((request, result) => {
+  result.write('Hello Holberton School!');
+  result.end();
+});
+
+app.listen((1245), () => {});
