4706061dec3a22ec7081ebd04e9e57b07cdaff27
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-09-20 10:08:03 +0200

    fixin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
 const readline = require('readline');
+
 const lineReader = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
+
 lineReader.question('Welcome to Holberton School, what is your name?\n', (username) => {
-  console.log('Your name is: ' + username);
+  console.log(`Your name is: ${username}`);
 });
 
 lineReader.on('close', () => {
