613040185152b33ea10f71b9e190312a8961fa47
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-09-20 10:14:17 +0200

    task 5

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 const readline = require('node:readline');
 
-const r = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-r.question('Welcome to Holberton School, what is your name?\n', (name) => {
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
 });
-r.on('close', () => {
+rl.on('close', () => {
   console.log('This important software is now closing');
 });
